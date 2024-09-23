907f39c103432252a75e6bda70dabc0a3bb05ce0
bartojer <bartojer@gmail.com>
2024-09-23 15:43:51 -0600

    Adding to the Prep1 so that it tells you your name in a cool way.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+        
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        Console.WriteLine();
+
+        Console.WriteLine($"Your name is {lname}, {fname} {lname}.");
     }
+
+
+
+
+
+
+
+
+
+
+
 }
+//Blond guy in the corner, taking calculus next semester - named Roger. Seems sharp. I think he works a lot.
