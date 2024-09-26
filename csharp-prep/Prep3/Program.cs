ee97fb31a2b38f81f19d858c87c52ff69ccc6dbf
bartojer <bartojer@gmail.com>
2024-09-26 10:39:26 -0600

    Added "Guess the Magic Number"

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,56 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // List<int>numberList = [1,2,3];
+        // foreach (int item in numberList)
+        // {
+        //     Console.WriteLine(item);
+        // }
+
+        // List<int> numberList2 = new();
+
+        // int input = -1;
+        // while (input != 0);
+        // {
+        //     Console.WriteLine("Enter an integer: ");
+        //     string inputString = Console.ReadLine();
+        //     input = int.Parse(inputString);
+        //     if (input != 0)
+        //     {
+        //         numberList2.Add(input);
+        //     }
+        // }
+        // foreach (int item in numberList2)
+        // {
+        //     Console.WriteLine(item);
+        // }
+        
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1,2049);
+
+        int numGuess = 0;
+        while (numGuess != magicNumber)
+        {
+            Console.WriteLine("Guess the magic number: ");
+            string guess = Console.ReadLine();
+            numGuess = int.Parse(guess);
+
+            if (numGuess < magicNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if (numGuess > magicNumber)
+            {
+                Console.WriteLine("Too high");
+            }
+            else
+            {
+                Console.WriteLine("Yay");
+            }
+        }
     }
 }
