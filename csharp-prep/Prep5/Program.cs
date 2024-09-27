db4d08b7e0cc2111b8d1ee560ccbca25a5d1b7b4
bartojer <bartojer@gmail.com>
2024-09-27 12:18:58 -0600

    Prompt and return name and favorite number (after squaring it).

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,11 +4,6 @@ using System.Reflection;
 
 class Program
 {
-    static double CalcNumSquare(double number)
-    {
-        double square = number * number;
-        return square;
-    }
 
 // DisplayWelcome - Displays the message, "Welcome to the Program!"
     static void DisplayWelcome()
@@ -17,35 +12,40 @@ class Program
     }
 
 // PromptUserName - Asks for and returns the user's name (as a string)
-    static void PromptUserName()
+    static string PromptUserName()
     {
-        Console.WriteLine("Please enter your name: ");
+        Console.Write("Please enter your name: ");
         string name = Console.ReadLine();
-        Console.WriteLine(name);
+        return name;
     }
 // PromptUserNumber - Asks for and returns the user's favorite number (as an integer)
-    static void PromptUserNumber()
+    static int PromptUserNumber()
     {
-        Console.WriteLine("Please enter your favorite number: ");
+        Console.Write("Please enter your favorite number: ");
         string inpNumber = Console.ReadLine();
-        double number = double.Parse(inpNumber);
-        double squared = CalcNumSquare(number);
-        Console.WriteLine(squared);
+        int number = int.Parse(inpNumber);
+        return number;
     }
 // SquareNumber - Accepts an integer as a parameter and returns that number squared (as an integer)
-// DisplayResult - Accepts the user's name and the squared number and displays them.
+    static int SquareNumber(int number)
+    {
+        int square = number * number;
+        return square;
+    }
 
+// DisplayResult - Accepts the user's name and the squared number and displays them.
+    static void DisplayResult(string name, int squaredNumber)
+    {
+        Console.WriteLine($"{name}, the square of your number is {squaredNumber}.");
+    }
     
     static void Main(string[] args)
     {
         DisplayWelcome();
-
-
-
-
-
-
-
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+        int squaredNumber = SquareNumber(number);
+        DisplayResult(name, squaredNumber);
 
 
 
