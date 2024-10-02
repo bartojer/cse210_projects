0d0c94e58381acb0df6a509c02c9588d9cfba650
bartojer <bartojer@gmail.com>
2024-10-02 15:01:43 -0600

    Co-authored-by: Andrew-J-Seaman <Andrew-J-Seaman@users.noreply.github.com> Co-authored-by: Grant Matai Cross <GMCRM@users.noreply.github.com>

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,91 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Make Journal");
     }
 }
+
+//yall here?
+// sure am!
+//Yippee!!!
+
+
+
+class MakeJournal
+{
+    
+// FUNCTION REQUIREMENTS: 
+
+    //write new entry
+
+    //display journal
+
+    //save journal to file
+    
+    //load journal
+
+    //provide menu
+
+    //provide prompts
+
+}
+
+//DESIGN PHILOSOPHY:develop the simplest yet best built out journal app.
+
+/* MENU
+    1. Write
+    2. Display // Or call 'View Entries'
+    3. Load
+    4. Save
+    5. Quit
+*/
+
+/* CLASSES:
+    1. Jounal
+        - Display
+        - Load
+    2. Entry
+        - Write
+        - Save 
+    3. PromptGenerator
+        - (In tandem with 'Write')
+*/
+
+/* USER EXPERIENCE - AVENUES:
+    A. Write only
+        1. Open app
+        2. Write (random prompt)
+        3. Save
+        4. Quit
+    B. View only
+        1. Open app
+        2. Load
+        3. Display
+        4. Quit
+    C. Load & write only
+        1. Open app
+        2. Load
+        3. Write
+        4. Save
+        5. Quit
+    D. Open & quit only
+        1. Open app
+        2. Quit app
+    E.
+        1. Open app
+        2. Load (read)
+        3. Quit
+    F:
+        1. 
+        2. 
+        3.
+        4.
+        5.
+    G:
+        1.
+        2.
+        3.
+        4.
+        5.
+
+*/
