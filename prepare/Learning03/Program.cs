0aac7df3fdb105c23a1c9d19092a42d9c802ad7e
bartojer <bartojer@gmail.com>
2024-10-15 15:36:12 -0600

    Added fraction making and calculating class

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,41 @@
 using System;
+using System.Diagnostics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction frac1 = new Fraction();
+        Fraction frac2 = new Fraction(15);
+        Fraction frac3 = new Fraction(5, 16);
+
+        //fraction 1
+        Console.WriteLine("fraction 1");
+        frac1.SetTop(15);
+        frac1.SetBottom(9);
+        Console.WriteLine(frac1.GetFractionString());
+        Console.WriteLine(frac1.GetDecimalValue());
+
+        //fraction 2
+        Console.WriteLine("fraction 2 - 1");
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+
+        frac2.SetTop(5);
+        frac2.SetBottom(15);
+        Console.WriteLine("fraction 2 - 2");
+        Console.WriteLine(frac2.GetFractionString());
+        Console.WriteLine(frac2.GetDecimalValue());
+
+        //fraction 3
+        Console.WriteLine("fraction 3 - 1");
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
+        
+        frac3.SetTop(95);
+        frac3.SetBottom(4);
+        Console.WriteLine("fraction 3 - 2");
+        Console.WriteLine(frac3.GetFractionString());
+        Console.WriteLine(frac3.GetDecimalValue());
     }
 }
